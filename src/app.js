25d5275ea04cddb84127098125b6b643233c93ee
Ferry van Maurik <ferryvanmaurik@msn.com>
2017-05-03 07:46:19 +0200

    Setup Firebase client

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './components/common' 
+import firebase from 'firebase';
+import { Header } from './components/common';
+
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+        apiKey: "AIzaSyB31bnHbnI5Ruy11ZXA-YJ4ztb8GEPk5yc",
+        authDomain: "auth-34487.firebaseapp.com",
+        databaseURL: "https://auth-34487.firebaseio.com",
+        projectId: "auth-34487",
+        storageBucket: "auth-34487.appspot.com",
+        messagingSenderId: "868604731849"
+  });
+}
     render() {
         return (
             <View>

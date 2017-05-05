f1f1bf9ac115b87c8f93f2a77b5715d723734b28
Ferry van Maurik <ferryvanmaurik@msn.com>
2017-05-05 17:19:23 +0200

    More conditional rendering

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, CardSection, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 
@@ -26,11 +26,27 @@ class App extends Component {
       }
   });
 }
+
+
+renderContent() {
+    switch (this.state.loggedIn) {
+        case true:
+            return <Button>Log out</Button>
+        case false:
+            return <LoginForm />
+        default:
+            return <Spinner size= "large" />
+    }
+}
+
+
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                <CardSection>
+                {this.renderContent()}
+                </CardSection>
             </View>
         );
     }
